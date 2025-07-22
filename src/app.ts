775553c32430cb3f6a48d5a41df0993dd1029950
BekDevIOS <umekhmonboev@gmail.com>
2025-07-22 15:36:11 +0900

    feat: setup morgan

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import path from 'path';
 import routerAdmin from './router-admin';
+import morgan from 'morgan';
+import { MORGAN_FORMAT } from './libs/config';
 
 // Entrance
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(morgan(MORGAN_FORMAT));
 
 
 
