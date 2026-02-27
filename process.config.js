c727acca3083877a128d746ad523bf05d4d6a15b
Abbos-Amirov <abbosamirov571@gmail.com>
2026-02-27 20:14:06 +0900

    fix: changes deploy.sh

--- process.config.js
+++ process.config.js
@@ -7,9 +7,11 @@ module.exports = {
       watch: false,
       env_production: {
         NODE_ENV: "production",
+        MONGO_URI: "mongodb+srv://Oscar:A3mzJqupDes8QLkx@cluster0.dbpygr2.mongodb.net/zomin",
       },
       env_development: {
         NODE_ENV: "development",
+        MONGO_URI: "mongodb+srv://Oscar:A3mzJqupDes8QLkx@cluster0.dbpygr2.mongodb.net/zomin",
       },
       instances: 1,
       exec_mode: "cluster",
